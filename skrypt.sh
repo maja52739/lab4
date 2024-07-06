2f123daaa21872d693ed99ae9889ed0b03b3dd95
maja52739 <dsw52739@student.dsw.edu.pl>
2024-07-06 22:58:54 +0200

    dodanie skrypt.sh --logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 if [ "$1" == "--date" ]; then
 	date
+
+elif [ "$1" == "--logs" ]; then
+	for i in $(seq 1 100); do
+		filename = "log${i}.txt"
+		echo "Nazwa pliku: $filename" > $filename
+		echo "Nazwa skryptu: "$0" >> $filename
+		echo "Data utworzenia: $(date)" >> $filename
+	done
 fi
