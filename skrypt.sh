eae5a1c8d4b92254c876e256f423a5e7f777d550
maja52739 <dsw52739@student.dsw.edu.pl>
2024-07-06 23:03:49 +0200

    modyfikacja --logs

--- skrypt.sh
+++ skrypt.sh
@@ -3,7 +3,12 @@ if [ "$1" == "--date" ]; then
 	date
 
 elif [ "$1" == "--logs" ]; then
-	for i in $(seq 1 100); do
+	if [[ -n "$2" ]]; then
+		num=$2
+	else
+		num=100
+	fi
+	for i in $(seq 1 num); do
 		filename = "log${i}.txt"
 		echo "Nazwa pliku: $filename" > $filename
 		echo "Nazwa skryptu: "$0" >> $filename
